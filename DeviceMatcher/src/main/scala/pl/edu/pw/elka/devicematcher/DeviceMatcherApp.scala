16929a0003ac7fcd066b8e7d5318e3d9a4a4fe43
sgalazka <szymon.galazka93@gmail.com>
2016-12-21 16:31:22 +0100

    Inicjalizacja repozytorium z kodem zrodlowym

---
+++ DeviceMatcher/src/main/scala/pl/edu/pw/elka/devicematcher/DeviceMatcherApp.scala
@@ -0,0 +1,12 @@
+package pl.edu.pw.elka.devicematcher
+
+/**
+ * Hello world!
+ *
+ */
+object DeviceMatcherApp extends App{
+
+  override def main(args: Array[String]) = {
+    println( "Hello World!" )
+  }
+}
